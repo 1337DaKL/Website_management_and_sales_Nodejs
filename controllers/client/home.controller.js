fbf998f37e48d6493568330ff78dec39b82530bd
1337DaKL <luongtrinh2k3ndad@gmail.com>
2024-12-31 00:27:26 +0700

    sửa lại tự động gửi gmail

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,15 +3,12 @@ const { model } = require("mongoose");
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.PASS_EMAIL
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
+    }
 });
 
 module.exports.index = (req, res) => {
@@ -42,6 +39,7 @@ module.exports.contact = async (req, res) => {
             }
             else
             {
+                console.log(error)
                 req.flash("error" , error)
                 res.redirect("back");
             }
