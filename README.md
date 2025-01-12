bc4183d159a181119a84d65b30b6e31355b8db77
1337DaKL <luongtrinh2k3ndad@gmail.com>
2025-01-12 15:31:06 +0700

    phân quyền từng trang

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Link trang web demo thực tế => https://website-management-and-sales-nodejs-6yf8rtwce.vercel.app/
+Link trang web demo thực tế => https://website-management-and-sales-nodejs-r7a78kqwg.vercel.app/
+
+Link admin => https://website-management-and-sales-nodejs-r7a78kqwg.vercel.app/admin/dashboard
+
+Tài khoản xem trang admin:
+
+Tk : admin@gmail.com
+mk: admin
--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,6 +1,7 @@
 const prefixSystem = require("../../configs/systems");
 const prefixAdmin = prefixSystem.prefixAdmin;
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model.js");
 module.exports.requireAuth = async (req, res, next) => {
     if (!req.cookies.token) {
         req.flash("error", "Bạn cần phải đăng nhập đi zzz");
@@ -10,9 +11,14 @@ module.exports.requireAuth = async (req, res, next) => {
         const account = await Account.findOne({
             deleted : false,
             token : req.cookies.token
-        })
+        }).select("-password -token");
         if(account)
         {
+            const role = await Role.findOne({
+                deleted : false,
+                _id : account.idRole
+            }).select("title permission");
+            res.locals.roleLogin = role;
             res.locals.userLogin = account;
             next();
         }
